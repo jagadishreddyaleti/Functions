a89a1363fdf3ecd053b6961be2fb0bfdf22217b1
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-15 14:12:03 +0530

    Add files via upload

---
+++ Functions basic.py
@@ -0,0 +1,61 @@
+""" functions """
+
+# def greet():
+#     print("morning")
+# greet()    
+
+
+# def greet():
+#     print("morning")
+#     greet()
+# greet()                      # function in function gives recursion error
+
+
+# def greet(n):
+#     print('hi ' +n)
+# greet('mama')     # passing 1 arguement in defining and calling 1 arguement while calling
+
+# def greet(n):
+#     print('hi ' +n)
+# greet('mama')  
+# greet('ra')  
+# greet('4')  
+# greet(4)  # cant concatenate
+
+# def greet(*names):
+#     print(['hey','hello','good','bye'])
+# greet('names')    
+
+# def greet(*names):
+#     print('hey',names[2])
+# greet('python','prorgam','core','functions')  
+
+
+# def details(n,a):
+#     print('your name is '+ n +  ' your age is '+  str(a))
+# details('jagadish',21)    
+# details(n='jagadish',a=21)    
+# details(a=21,n='jagadish')  
+
+# def details(n,a):
+#     print('your name is '+ n +  ' your age is '+  str(a))
+# n=input('enter your name : ')
+# a=input('enter your age: ')
+# details(n,a)  
+
+# def a(x,y):
+#     return print(x*y)
+# a(5,8)  
+
+# def a(x,y):
+# #     return (x*y)
+# # print(a(5,6))
+
+# def z(p,q):
+#     return p+q,p*q
+# p=int(input())
+# q=int(input())
+# print(z(p,q))
+
+
+
