9c2351299168a9436d08e7d4ae9f45c7322639dd
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-17 22:36:01 +0530

    Add files via upload

---
+++ super keyword.py
@@ -0,0 +1,15 @@
+# class A:
+#     def method1(self):
+#         print(' i am parent class')
+
+# class B(A):
+#     def method1(self):
+#         super().method1()
+#         print(' i am child class') 
+#         super().method1() 
+
+# C=B()
+# C.method1()
+
+
+               
