5810c7055fee09481f64bf907daf9bfbf8bf7db3
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-24 17:48:37 +0530

    Add files via upload

---
+++ sign up login programm.py
@@ -0,0 +1,9 @@
+# usr=input('enter your user name: ')
+# pwd=input('enter your password: ')
+# print('account created')
+# name=input('enter your user name: ')
+# psw=input('enter your password: ')
+# if (usr==name and pwd==psw):
+#     print('you have logged in sucessfully')
+# else:
+#     print('Invalid Details')    
